40968cae86ef555afe5c8b8779d0fc800660eb9a
harit1412 <202103039@daiict.ac.in>
2022-01-25 22:53:47 +0530

    fun

--- PYimageeditor.py
+++ PYimageeditor.py
@@ -1,14 +1,18 @@
 from asyncio.windows_events import NULL
 from tkinter import *
+from tkinter.messagebox import askyesno
 from PIL import Image,ImageTk,ImageEnhance
 # from tkinter import Image
 import tkinter as tk
 from tkinter import filedialog
 from tkinter.filedialog import askopenfile
 from buttons import buttons
-from tkinter.filedialog import askopenfile
+
 import ctypes
 import colorsys
+import cv2
+
+
 
 
 ctypes.windll.shcore.SetProcessDpiAwareness(1000)
@@ -19,6 +23,26 @@ root.geometry("1000x1000")
 
 my_canvas1 = Canvas(root,width=1220,height=780, bg="lightgray")
 my_canvas1.place(x=320,y=30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
@@ -42,9 +66,14 @@ def select_crop():
 
 
 def crop():
-    pass
-  
-  
+    global new
+    global im ,im_g,i
+    global label 
+    im_g= im.resize((1200,780))
+    i = im_g.crop([200,500,600,300])
+    new = ImageTk.PhotoImage(i)
+    label.config(image=new)
+
 def flip_vert():
     global new
     global im ,im_g
@@ -75,18 +104,138 @@ def BnW():
 my_canvas = Canvas(root, width=00,height=900 ,bg="white")
 my_canvas.place(x=0,y=30)
 
+# -----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
+
+
+def Sat_in(self):
+  global im,new,label,im_g,img
+  global a
+  filter = ImageEnhance.Color(im)
+  a = satu_scale.get()
+  img = filter.enhance(a)
+  im_g = img.resize((1200,780))
+  new = ImageTk.PhotoImage(im_g)
+  label.configure(image=new)
 
 def Saturation():
-    global im,new,label,im_g
-    filter = ImageEnhance.Color(im)
-    img = im.filter(3)
+    global ans
+    ans = askyesno(title = "Confirmation",message = "Do you want to change?")  
+    if ans : 
+        pass
+       
+    else :
+        # satu_scale.set("0")
+        global im,new,label,im_g,img
+        im_resize = im.resize((1200,780))
+        img = ImageTk.PhotoImage(im_resize) 
+        label.configure(image=img)
+        
+    
+satu_scale = Scale(my_canvas, from_= -100 , to=100, orient = 'horizontal',bg="yellow",fg="black",command = Sat_in)
+satu_scale.grid(row=4,column=1,padx=2,pady=5)
+
+# -----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
+
+
+def sharp_in(self):  
+    global im,new,label,im_g,img,b
+    b = sharp_scale.get()
+    filter = ImageEnhance.Sharpness(im)
+    img = filter.enhance(b)
     im_g = img.resize((1200,780))
     new = ImageTk.PhotoImage(im_g)
     label.configure(image=new)
     
 
 
+def sharp():
+    global ans
+    ans = askyesno(title = "Confirmation",message = "Do you want to change?")  
+    if ans : 
+        pass
+       
+    else :
+        global im,new,label,im_g,img
+        im_resize = im.resize((1200,780))
+        img = ImageTk.PhotoImage(im_resize) 
+        label.configure(image=img)    
+        sharp_scale.set("0")
 
+
+
+
+sharp_scale = Scale(my_canvas, from_= 0 , to=100, orient = 'horizontal',bg="yellow",fg="black",command =sharp_in)
+sharp_scale.grid(row=5,column=1,padx=2,pady=5)
+
+# -----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
+
+
+def cont():
+    global ans
+    ans = askyesno(title = "Confirmation",message = "Do you want to change?")  
+    if ans : 
+        pass
+        
+    else :
+        global im,new,label,im_g,img
+        im_resize = im.resize((1200,780))
+        img = ImageTk.PhotoImage(im_resize) 
+        label.configure(image=img)
+        cont_scale.set("0")
+
+
+def cont_scale(self):
+    global cont_scale,b,img,im_g,new,label
+    b = cont_scale.get()
+    filter = ImageEnhance.Contrast(im)
+    img = filter.enhance(b)
+    im_g = img.resize((1200,780))
+    new = ImageTk.PhotoImage(im_g)
+    label.configure(image=new)
+
+
+cont_scale = Scale(my_canvas, from_= -100 , to=100, orient = 'horizontal',bg="yellow",fg="black",command=cont_scale)
+cont_scale.grid(row=6,column=1,padx=2,pady=5)
+
+
+# -----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
+
+
+def Expo_in(event):
+  global im,new,label,im_g,img
+  global a
+  a = satu_scale.get()
+  filter = ImageEnhance.Brightness(im)
+  img = filter.enhance(a)
+  im_g = img.resize((1200,780))
+  new = ImageTk.PhotoImage(im_g)
+  label.configure(image=new)
+
+def expo():
+    global ans
+    ans = askyesno(title = "Confirmation",message = "Do you want to change?")  
+    
+    if ans : 
+        pass
+       
+    else :
+        # satu_scale.set("0")
+        global im,new,label,im_g,img
+        im_resize = im.resize((1200,780))
+        img = ImageTk.PhotoImage(im_resize) 
+        label.configure(image=img)
+        
+    
+expo_scale = Scale(my_canvas, from_= 2 , to= 100, orient = 'horizontal',bg="yellow",fg="black",command = Expo_in)
+expo_scale.set("2")
+expo_scale.grid(row=7,column=1,padx=2,pady=5)
+
+# -----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
 
 
 
@@ -99,7 +248,9 @@ open.grid(row=0,column=1,padx=2,pady=2,ipadx=10)
 
 #helllo hhhh
 
-buttons(my_canvas,crop,select_crop,flip_hori,flip_vert,BnW,Saturation)
+buttons(my_canvas,crop,select_crop,flip_hori,flip_vert,BnW,Saturation,sharp,cont,expo)
+
+
 
 img = PhotoImage(file="abcd.png",width=1300,height=900)
 my_image = my_canvas1.create_image(200,200 , anchor = NW , image = img)
--- buttons.py
+++ buttons.py
@@ -1,6 +1,6 @@
 from tkinter import *
 class buttons :
-    def __init__(self,my_canvas,crop,select_crop,flip_hori,flip_vert,BnW,Saturation):
+    def __init__(self,my_canvas,crop,select_crop,flip_hori,flip_vert,BnW,Saturation,sharp,cont,expo):
       self.BW = Button(my_canvas, text="Conver to Black & White", bg = "yellow", fg = "black",command = BnW)
       self.BW.grid(row=0, column=0,padx=2,pady=5)
       ##
@@ -18,38 +18,38 @@ class buttons :
       self.flip_vert.grid(row=3,column=0,padx=2,pady=5)
       ##
       
-      global a
-      self.satu = Button(my_canvas, text="Apply Saturation",bg="yellow",fg="black",command = Saturation)
-      self.satu_in = Scale(my_canvas, from_= -100 , to=100, orient = 'horizontal',bg="yellow",fg="black")
-      a = self.satu_in.get()      
+      # global a
+      self.satu = Button(my_canvas, text="Apply Saturation",bg="yellow",fg="black",command =Saturation )
+      # self.satu_in = Scale(my_canvas, from_= -100 , to=100, orient = 'horizontal',bg="yellow",fg="black",command = Saturation)
+      # self.a = self.satu_in.get() 
 
       self.satu.grid(row=4,column=0,padx=2,pady=5)
-      self.satu_in.grid(row=4,column=1,padx=2,pady=5)
+      # self.satu_in.grid(row=4,column=1,padx=2,pady=5)
       ##
       
-      self.sharp = Button(my_canvas, text="Apply Sharpness",bg="yellow",fg="black")
-      self.sharp_in = Scale(my_canvas, from_= 0 , to=100, orient = 'horizontal',bg="yellow",fg="black")
+      self.sharp = Button(my_canvas, text="Apply Sharpness",bg="yellow",fg="black",command=sharp)
+      # self.sharp_in = Scale(my_canvas, from_= 0 , to=100, orient = 'horizontal',bg="yellow",fg="black")
       
       
       self.sharp.grid(row=5,column=0,padx=2,pady=5)
-      self.sharp_in.grid(row=5,column=1,padx=2,pady=5)
+      # self.sharp_in.grid(row=5,column=1,padx=2,pady=5)
       ##
       
-      self.cont = Button(my_canvas, text="Apply Contrast",bg="yellow",fg="black")
-      self.cont_in = Scale(my_canvas, from_= 0 , to=100, orient = 'horizontal',bg="yellow",fg="black")
+      self.cont = Button(my_canvas, text="Apply Contrast",bg="yellow",fg="black",command= cont)
+      # self.cont_in = Scale(my_canvas, from_= 0 , to=100, orient = 'horizontal',bg="yellow",fg="black")
       # self.cont_in.get()
       
       
       self.cont.grid(row=6,column=0,padx=2,pady=5)
-      self.cont_in.grid(row=6,column=1,padx=2,pady=5)
+      # self.cont_in.grid(row=6,column=1,padx=2,pady=5)
       ##
       
-      self.expo = Button(my_canvas, text="Apply Exposure",bg="yellow",fg="black")
-      self.expo_in = Scale(my_canvas, from_= 0 , to=100, orient = 'horizontal',bg="yellow",fg="black")
+      self.expo = Button(my_canvas, text="Apply Exposure",bg="yellow",fg="black",command= expo)
+      # self.expo_in = Scale(my_canvas, from_= 0 , to=100, orient = 'horizontal',bg="yellow",fg="black")
       
       
       self.expo.grid(row=7,column=0,padx=2,pady=5)
-      self.expo_in.grid(row=7,column=1,padx=2,pady=5)
+      # self.expo_in.grid(row=7,column=1,padx=2,pady=5)
       
       self.highlight = Button(my_canvas, text="Highlight Border",bg="yellow",fg="black")
       self.highlight.grid(row=8,column=0,padx=2,pady=5)
@@ -65,5 +65,6 @@ class buttons :
       
       self.fix_text = Button(my_canvas, text="Fix Poaition",bg="yellow",fg="black")
       self.fix_text.place(x= 144 , y= 407) 
-             
+
+     
       
